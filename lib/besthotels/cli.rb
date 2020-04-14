3a469cb4d46d1bab44125fc1e0a2c2379e0537b2
bob <bob@ww>
2020-04-13 20:29:44 -0500

    Done.

--- lib/besthotels/cli.rb
+++ lib/besthotels/cli.rb
@@ -1,16 +1,19 @@
 class Besthotels::CLI 
 
-    def call
+    def self.call
         puts "Welcome , user"
         puts "Do you want to see best hotels for 2020,if Yes ,please enter: 'Yes' "
         puts "if you like to exit ,enter:'exit'"
-        menue
+        self.menue
      end
   
-     def menue 
+     def self.menue 
          user= gets.chomp
         if user == "Yes"
          self.listhotel
+         
+         #binding.pry
+         
          puts "There is a list "
          puts "for more detalis about each hotel ,choose the number from 1 to 25"
          
@@ -23,17 +26,17 @@ class Besthotels::CLI
         end
      end
   
-     def  exit
+     def  self.exit
          "See You"
      end
   
-     def invalid
+     def self.invalid
         puts"invalid input ,please enter valid option"
         self.menue
      end
   
-     def listhotel
-      aa= Besthotels::Hotels.all
+     def self.listhotel
+      aa= Besthotels::Hotels.all          
         aa.each_with_index do |value,i|
           puts "#{i+1}. #{value.hotel}.      #{value.location} "
         end
